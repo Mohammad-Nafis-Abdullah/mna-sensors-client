5db58e96f5f750fdf1b2e676e98aa9bfbd2466e9
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-05-24 12:34:24 +0600

    purchase form state and quantity error updated

--- src/components/private/purchase/Purchase.js
+++ src/components/private/purchase/Purchase.js
@@ -44,53 +44,54 @@ const Purchase = () => {
 
     return (
         <div className='container min-h-[calc(100vh-166.5px)] flex flex-wrap justify-center items-center px-3 py-8 gap-8'>
-            <form onSubmit={checkedOut} className='max-w-sm p-5 rounded-xl shadow-[0_0_15px_1px_rgba(0,0,0,0.343)] w-full fromLeft'>
-                <h3 className='text-center mb-10 text-gray-800 font-medium text-3xl'>Purchase Form</h3>
-                <div className="input-container">
-                    <input type="text" name="name" className="input-field" placeholder={user.displayName} required="" disabled />
-                    <label className="input-label">Name</label>
-                </div>
-                <div className="input-container">
-                    <input type="email" name="email" className="input-field" placeholder={user.email} required="" disabled />
-                    <label className="input-label">Email address</label>
-                </div>
-                <div className="input-container">
-                    <input type="tel" name="phone" className="input-field" placeholder=" " required="" />
-                    <label className="input-label">Phone number</label>
-                </div>
-                <div className="input-container">
-                    <input type="text" name="address" className="input-field" placeholder=" " required="" />
-                    <label className="input-label">Address</label>
-                </div>
+            <form onSubmit={checkedOut} className='p-5 rounded-xl shadow-[0_0_15px_1px_rgba(0,0,0,0.343)] w-full flex flex-wrap justify-center items-center gap-x-16 gap-y-5'>
+                <h3 className='text-center text-gray-800 font-medium text-3xl basis-full'>Purchase Form</h3>
+                <section className='fromLeft max-w-sm w-full flex flex-col justify-center items-center p-3 rounded-xl gap-3'>
+                    <div className="input-container">
+                        <input type="text" name="name" className="input-field" placeholder={user.displayName} required="" disabled />
+                        <label className="input-label">Name</label>
+                    </div>
+                    <div className="input-container">
+                        <input type="email" name="email" className="input-field" placeholder={user.email} required="" disabled />
+                        <label className="input-label">Email address</label>
+                    </div>
+                    <div className="input-container">
+                        <input type="tel" name="phone" className="input-field" placeholder=" " required />
+                        <label className="input-label">Phone number</label>
+                    </div>
+                    <div className="input-container">
+                        <input type="text" name="address" className="input-field" placeholder=" " required />
+                        <label className="input-label">Address</label>
+                    </div>
+                </section>
                 {
-                    selected ?
-                        <div className='max-w-sm w-full flex flex-col justify-center items-center p-3 shadow-[0_0_15px_1px_rgba(0,0,0,0.343)] rounded-xl gap-3'>
+                    selected &&
+                        <div className='max-w-sm w-full flex flex-col justify-center items-center p-3 shadow-[0_0_15px_1px_rgba(0,0,0,0.343)] rounded-xl gap-3 fromRight'>
                             <img className='h-28 w-36 object-cover rounded-md' src={selected.img} alt="" />
                             <div className='text-center font-medium space-y-3 grow'>
                                 <h5 className='text-gray-800 text-left text-xl'>{selected.name}</h5>
                                 <div className="input-container">
-                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-lg'>Price per unit :<input type="text" className="input-field basis-1/2 text-center" placeholder={`$ ${selected.unitPrice}`} disabled /></label>
+                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-sm sm:text-lg'>Price per unit :<input type="number" className="input-field basis-1/2 text-center" placeholder={`$ ${selected.unitPrice}`} disabled /></label>
                                 </div>
                                 <div className="input-container">
-                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-lg'>Minimum Order :<input type="text" className="input-field basis-1/2 text-center" placeholder={selected.minQuantity} disabled /></label>
+                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-sm sm:text-lg'>Minimum Order :<input type="number" className="input-field basis-1/2 text-center" placeholder={selected.minQuantity} disabled /></label>
                                 </div>
                                 <div className="input-container">
-                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-lg'>Quantity :<input type="number" className="input-field basis-1/2 text-center text-xl font-medium" placeholder='' required=" " onBlur={checkQuantity} onChange={quantifing} value={quantity || selected.minQuantity} /></label>
+                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-sm sm:text-lg'>Quantity :<input type="number" className="input-field basis-1/2 text-center text-xl font-medium" placeholder='' required=" " onBlur={checkQuantity} onChange={quantifing} value={quantity || selected.minQuantity} /></label>
+                                    {
+                                        (quantity<selected.minQuantity || quantity>selected.availableQuantity) && <small className='text-red-600'>{`Enter quantity between ${selected.minQuantity} to ${selected.availableQuantity}`}</small>
+                                    }
                                 </div>
                                 <div className="input-container">
-                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-lg'>Total Available :<input type="text" className="input-field basis-1/2 text-center" placeholder={selected.availableQuantity} disabled /></label>
+                                    <label htmlFor="" className='flex justify-between items-center gap-2 text-sm sm:text-lg'>Total Available :<input type="number" className="input-field basis-1/2 text-center" placeholder={selected.availableQuantity} disabled /></label>
                                 </div>
-                            </div>
-                        </div> :
-                        <div className="alert alert-error shadow-lg">
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                <span className='uppercase'>error! product not found.</span>
                             </div>
                         </div>
                 }
-                <button type='submit' className="block mt-10 mx-auto px-16 py-1.5 rounded-md text-gray-800 font-medium
+                <div className='basis-full'>
+                    <button type='submit' className="block mx-auto px-16 py-1.5 rounded-md text-gray-800 font-medium
                 hover:text-white hover:bg-gray-800 border-2 border-gray-800 duration-150 active:scale-95">Order</button>
+                </div>
             </form>
         </div>
     );
